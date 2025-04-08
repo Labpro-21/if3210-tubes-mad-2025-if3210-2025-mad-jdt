8971b808a0d217d94e79d83fd13d318ba4380445
satriadhikara <84092642+satriadhikara@users.noreply.github.com>
2025-04-08 14:24:08 +0700

    fix: change compile and target sdk

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "com.purrytify.mobile"
-    compileSdk = 35
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.purrytify.mobile"
         minSdk = 29
-        targetSdk = 35
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
